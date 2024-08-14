68fe09607a1faf698787cfbdb186cc69eb91acd8
PAUL STEPHANE <stephanepaul191@gmail.com>
2024-08-14 11:34:46 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Gallery page loaded successfully.");
+    // Add any additional JavaScript functionality here
+});
+
+file:///media/paul/Nouveau%20nom/ASAP%20Food%20trailer%20and%20truck%20for%20sale%20and%20rent/prog.html/gallery.html
+file:///media/paul/Nouveau%20nom/ASAP%20Food%20trailer%20and%20truck%20for%20sale%20and%20rent/prog.html/gallery.html
